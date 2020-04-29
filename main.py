ae0e51da349f0e18794a937c0320d50d7624c841
karoo27 <swiecicka.karolina27@gmail.com>
2020-04-29 21:04:37 +0200

    Added export to csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import pyexcel as p
 import instalments as ins
 
 
@@ -12,14 +13,15 @@ print('Wpisz 1,by liczyć odsetki metodą 30/360, wpisz 2, by liczyć odsetki me
 typeOfIns=int(input())
 
 if typeOfIns==1:
-    test = ins.instalments30(capital, nbrOfIns, interestRate, datetime.date.today())
+    rozklad = ins.instalments30(capital, nbrOfIns, interestRate, datetime.date.today())
 elif typeOfIns==2:
-     test = ins.instalmentsACT(capital, nbrOfIns, interestRate, datetime.date.today())
+     rozklad = ins.instalmentsACT(capital, nbrOfIns, interestRate, datetime.date.today())
 else:
     print("Podałeś niepoprawny typ rozkładu odsetek!")
 
 #print("Kapitał: ", capital, " Liczba rat:", nbrOfIns, " Stopa:", interestRate, "%")
-for a in test:
+for a in rozklad:
     print(a)
 
+p.save_as(array=rozklad, dest_file_name='kap'+str(capital)+'_rat'+str(nbrOfIns)+'_stopa'+str(interestRate)+'.csv', dest_delimiter=";")
 
